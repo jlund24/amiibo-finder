917359fc3a9f33178b7b74cec2212a468aa0d95d
John Lund <johnrlund0@gmail.com>
2019-02-14 17:53:57 -0700

    switched to amiibo

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,45 @@
 $(document).ready(function() {
     
+    function buildCharacterCards(data)
+    {
+        html = "";
+        for (let i = 0; i < data.length; i++)
+        {
+            html += "<div class='amiibo-character-card' id='amiibo-character-card-" + i + "'>";
+            html += "<img class='amiibo-character-image' src='" + data[i].image + "'/>" 
+            html += "<div class='amiibo-character-name'>" + data[i].name + "</div>";
+            html += "<div class='amiibo-character-title'>" + data[i].character + "</div>";
+            html += "<div class='amiibo-character-amiibo-series'>" + data[i].amiiboSeries + "</div>";
+            html += "<div class='amiibo-character-game-series'>" + data[i].gameSeries + "</div>";
+            html += "<div class='amiibo-character-release-dates'>";
+                html += "<div class='release-date-us'>";
+                    html += "<img src='../images/us-flag.png'/>"
+                html += "</div>"; //end release-date-us
+            html += "</div>"; //end amiibo-character-release dates
+            html += "</div>"; //end amiibo-card
+        }
+        return html;
+        
+    }
+    
     function onResponse(json)
     {
         console.log(json);
+        results = "";
+        
+        results += buildCharacterCards(json.amiibo);
+        
+        document.getElementById("results").innerHTML = results;
+
     }
     document.getElementById("account-submit").addEventListener("click", function(event)
     {
         event.preventDefault();
         const value = document.getElementById("account-input").value;
         console.log(value);
-        const url = "https://haveibeenpwned.com/api/v2/breachedaccount/" + value;
-        fetch(url, {mode: "cors", headers : {"User-Agent": "Pwnage-Checker-For-iOS"}}).then(onResponse())
+        const url = "http://www.amiiboapi.com/api/amiibo/?character=" + value;
+        fetch(url).then(function(response) {
+            return response.json();
+            }).then(onResponse);
     });
 });
